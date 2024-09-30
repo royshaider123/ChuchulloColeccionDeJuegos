afd239fb1eb7737161e905589c4d47b91269d6e2
Roy Shaider Chuchullo Pucho <roy.chuchullo@tecsup.edu.pe>
2024-09-30 16:53:34 -0500

    Initial Commit

---
+++ ChuchulloColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChuchulloColeccionDeJuegos
+//
+//  Created by Roy Shaider Chuchullo Pucho on 30/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
